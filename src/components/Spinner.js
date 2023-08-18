c5bb2ea6934c6c09589ec05e1ccc7f9be9268b44
YASH SAWARKAR <104064673+Theyashsawarkar@users.noreply.github.com>
2023-08-18 20:19:26 +0530

    new loading animations are added

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,12 +1,26 @@
-import React from 'react'
-import "./Spinner.css"
+import React from "react";
+import "./Spinner.css";
 const Spinner = () => {
   return (
-    <div className="flex flex-col items-center space-y-2">
-      <div className='spinner'></div>
-      <p className="text-bgDark text-lg font-semibold">Loading....</p>
-    </div>
-  )
-}
+    // first animation
 
-export default Spinner
+    // <div className="flex flex-col items-center space-y-2">
+    //   <div className='spinner'></div>
+    //   <p className="text-bgDark text-lg font-semibold">Loading....</p>
+    // </div>
+
+    // second animation
+
+    // <div class="lds-ellipsis">
+    //   <div></div>
+    //   <div></div>
+    //   <div></div>
+    //   <div></div>
+    // </div>
+
+    // third animation
+    <div className="custom-loader"></div>
+  );
+};
+
+export default Spinner;
